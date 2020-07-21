491e0c0acd54f013a04ab7740a12b09a0fc11faa
= <dhananjaypanage11@gmail.com>
2020-07-22 02:11:52 +0530

    Uploading all the files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html', title="Home")
+
+@app.route('/teenager_prayer')
+def teenager_prayer():
+    return render_template('teenager_prayer.html')
+if __name__ == '__main__':
+    app.run(port=5000)
