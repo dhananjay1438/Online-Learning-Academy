116af7d2939aa5ee241ff3cb25cc0417bc07a818
= <dhananjaypanage11@gmail.com>
2020-08-09 18:15:56 +0530

    Added endpoint for added chapters (degree of comparison and language study)

--- app.py
+++ app.py
@@ -9,5 +9,19 @@ def home():
 @app.route('/teenager_prayer')
 def teenager_prayer():
     return render_template('teenager_prayer.html')
+
+@app.route('/be_smart')
+def be_smart():
+    return render_template('be_smart.html')
+
+@app.route('/degree_of_comparison')
+def degree_of_comparison():
+    return render_template('degree_of_comparison.html')
+
+@app.route('/language_study')
+def language_study():
+    return render_template('language_study.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
